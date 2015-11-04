39d8c4d47e20e0e7efef6747410ed21b6ed3f092
Phill Sparks <me@phills.me.uk>
2015-11-04 22:47:22 +0000

    Improve Vim-mode indicator

--- zsh/configs/vim-mode.zsh
+++ zsh/configs/vim-mode.zsh
@@ -1,9 +1,23 @@
-function zle-line-init zle-keymap-select {
-  VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]%  %{$reset_color%}"
-  export RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/}$EPS1"
+vim_ins_mode="∙"
+vim_cmd_mode="⋮"
+vim_mode=$vim_ins_mode
+
+function zle-keymap-select {
+  vim_mode="${${KEYMAP/vicmd/${vim_cmd_mode}}/(main|viins)/${vim_ins_mode}}"
   zle reset-prompt
 }
-
-zle -N zle-line-init
 zle -N zle-keymap-select
-export KEYTIMEOUT=1
+
+function zle-line-finish {
+  vim_mode=$vim_ins_mode
+}
+zle -N zle-line-finish
+
+# Fix a bug when you C-c in CMD mode and you'd be prompted with CMD mode indicator, while in fact you would be in INS mode
+# Fixed by catching SIGINT (C-c), set vim_mode to INS and then repropagate the SIGINT, so if anything else depends on it, we will not break it
+function TRAPINT() {
+  vim_mode=$vim_ins_mode
+  return $(( $1 - 1 ))
+}
+
+SLIMLINE_PROMPT_SYMBOL='${vim_mode}'
