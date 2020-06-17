c9af7b47fb54dca29bdbc9dc0b098d8a27a78e2e
Phill Sparks <me@phills.me.uk>
2020-06-17 21:36:40 +0100

    Remove node version from prompt

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,5 +1,5 @@
 SLIMLINE_LEFT_PROMPT_SECTIONS="vi_mode |default|"
-SLIMLINE_RIGHT_PROMPT_SECTIONS="execution_time exit_status git aws_profile virtualenv nodejs"
+SLIMLINE_RIGHT_PROMPT_SECTIONS="execution_time exit_status git aws_profile virtualenv"
 
 slimline=$(dirname $0)/../slimline/slimline.zsh
 [[ -f $slimline ]] && source $slimline
