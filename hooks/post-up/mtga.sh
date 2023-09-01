1a87082ec7f9b56200de1844e3c086a8fe196dca
Phill Sparks <me@phills.me.uk>
2023-09-01 17:17:39 +0100

    Override useful MTGA settings

---
+++ hooks/post-up/mtga.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+defaults write com.wizards.mtga "Screenmanager Fullscreen mode" -integer 0
+defaults write com.wizards.mtga "Screenmanager Resolution Use Native" -integer 0
+defaults write com.wizards.mtga "Screenmanager Resolution Width" -integer 2560
+defaults write com.wizards.mtga "Screenmanager Resolution Height" -integer 1440
